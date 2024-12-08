101a1167c907287977a3a9ac4af0f838c6ec433f
barefootstache <webmaster@barefootstache.com>
2024-12-08 01:33:40 +0100

    feat: generate preview automatically from existing icons

--- convert-inkscape-to-normal-svg.sh
+++ convert-inkscape-to-normal-svg.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 set -eux
 
+cp preview-template.html preview.html
+
 for i in icons-with-shapes/*.svg
 do
   fn_clean=$(basename -s .svg $i)
   fn=$(basename $i)
   inkscape --export-type=svg --export-plain-svg --export-filename=export/$fn $i
-  sed -i 's/width="180"/width="18"/g' export/$fn
-  sed -i 's/height="180"/height="18"/g' export/$fn
+  sed -i "s/width=\"180\"/width=\"18\"/g" export/$fn
+  sed -i "s/height=\"180\"/height=\"18\"/g" export/$fn
   cp export/$fn export/$fn_clean-green.svg
 #  cp export/$fn export/$fn_clean-blue.svg
+  icon_default="<img src=\"export/${fn_clean}.svg\" />"
+  icon_green="<img src=\"export/${fn_clean}-green.svg\" />"
+  icon_default_40="<img src=\"export/${fn_clean}.svg\" width=40 />"
+  icon_green_40="<img src=\"export/${fn_clean}-green.svg\" width=40/>"
+  sed -i "s|ICON_DEFAULT_0|${icon_default}\nICON_DEFAULT_0|" preview.html
+  sed -i "s|ICON_GREEN_0|${icon_green}\nICON_GREEN_0|" preview.html
+  sed -i "s|ICON_DEFAULT_40|${icon_default_40}\nICON_DEFAULT_40|" preview.html
+  sed -i "s|ICON_GREEN_40|${icon_green_40}\nICON_GREEN_40|" preview.html
 done
+
+sed -i "s|ICON_DEFAULT_0||" preview.html
+sed -i "s|ICON_GREEN_0||" preview.html
+sed -i "s|ICON_DEFAULT_40||" preview.html
+sed -i "s|ICON_GREEN_40||" preview.html
+
 
 #for i in export/*-blue.svg
 #do
@@ -20,7 +36,7 @@ done
 
 for i in export/*-green.svg
 do
-  sed -i 's/#444444/#00b400/g' $i
-  sed -i 's/#888888/#00b400/g' $i
+  sed -i "s/#444444/#00b400/g" $i
+  sed -i "s/#888888/#00b400/g" $i
 done
 
